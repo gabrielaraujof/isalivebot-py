8130d324e20c92c943aff115105ebb91378f4eda
Gabriel Araujo <gabrielferreira@ciandt.com>
2023-05-09 00:47:56 -0300

    add: is playing notfitication

--- isalivebot.py
+++ isalivebot.py
@@ -9,12 +9,19 @@ class IsAlive(discord.Client):
 
     async def on_voice_state_update(self, member, before, after):
         if not before.self_stream and after.self_stream:
-            activities = [activity for activity in member.activities if activity.type == discord.ActivityType.playing]
-            game_message = f'de {activities[0].name}' if activities else ''
-            notify(f'Oi pessoal, estou passando para avisar que {member.nick or member.name} acabou de abrir live {game_message} em {after.channel.name}') 
+            playing = discord.utils.get(member.activities, type=discord.ActivityType.playing)
+            game_message = f' de {playing.name}' if playing else ''
+            game_details = f' - {playing.details}' if playing and playing.details else ''
+            notify(f'Oi pessoal, estou passando para avisar que {member.nick or member.name} acabou de abrir live{game_message}{game_details} em {after.channel.name}') 
         
     async def on_presence_update(self, before, after):
-        print(f'{after.activities}')
+        before_playing = discord.utils.get(before.activities, type=discord.ActivityType.playing)
+        after_playing = discord.utils.get(after.activities, type=discord.ActivityType.playing)
+        if not before_playing and after_playing:
+            burgues = discord.utils.get(after.roles, name='Burguês')
+            if burgues:
+                game_details = f' - {after_playing.details}' if after_playing and after_playing.details else ''
+                notify(f'{after.nick or after.name} começou a jogar {after_playing.name}{game_details}')
 
 intents = discord.Intents(guilds=True, members=True, presences=True, voice_states=True)
 
