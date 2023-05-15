c2d1da685136a2fa41f25419d8364eb49fc72d69
Gabriel Araujo <gabrielferreira@ciandt.com>
2023-05-14 23:28:40 -0300

    fix: removes the presence update notification
    Since we are flooding the group with the updates, we are stepping back
    until we have a better solution

--- isalivebot.py
+++ isalivebot.py
@@ -13,15 +13,6 @@ class IsAlive(discord.Client):
             game_message = f' de {playing.name}' if playing else ''
             game_details = f' - {playing.details}' if playing and playing.details else ''
             notify(f'Oi pessoal, estou passando para avisar que {member.nick or member.name} acabou de abrir live{game_message}{game_details} em {after.channel.name}') 
-        
-    async def on_presence_update(self, before, after):
-        before_playing = discord.utils.get(before.activities, type=discord.ActivityType.playing)
-        after_playing = discord.utils.get(after.activities, type=discord.ActivityType.playing)
-        if not before_playing and after_playing:
-            burgues = discord.utils.get(after.roles, name='Burguês')
-            if burgues:
-                game_details = f' - {after_playing.details}' if after_playing and after_playing.details else ''
-                notify(f'{after.nick or after.name} começou a jogar {after_playing.name}{game_details}')
 
 intents = discord.Intents(guilds=True, members=True, presences=True, voice_states=True)
 
