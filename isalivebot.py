1bdd0fc745108909b27ca3e3ee7b2026d18e5360
Gabriel Araujo <gabrielferreira@ciandt.com>
2023-05-15 22:09:33 -0300

    add: voice state update for joining channel

--- isalivebot.py
+++ isalivebot.py
@@ -8,11 +8,24 @@ class IsAlive(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_voice_state_update(self, member, before, after):
-        if not before.self_stream and after.self_stream:
-            playing = discord.utils.get(member.activities, type=discord.ActivityType.playing)
-            game_message = f' de {playing.name}' if playing else ''
-            game_details = f' - {playing.details}' if playing and playing.details else ''
-            notify(f'Oi pessoal, estou passando para avisar que {member.nick or member.name} acabou de abrir live{game_message}{game_details} em {after.channel.name}') 
+        name = member.nick or member.name
+        playing = discord.utils.get(member.activities, type=discord.ActivityType.playing)
+        game_message = f' e está jogando {playing.name}' if playing else ''
+        game_details = f' - {playing.details}' if playing and playing.details else ''
+
+        if not before.channel and after.channel:
+            notify(f'{name} entrou em {after.channel.name}{game_message}{game_details}')
+        elif not before.self_stream and after.self_stream:
+            notify(f'{name} está ao vivo em {after.channel.name}{game_message}{game_details}') 
+
+    async def on_presence_update(self, before, after):
+        before_playing = discord.utils.get(before.activities, type=discord.ActivityType.playing)
+        after_playing = discord.utils.get(after.activities, type=discord.ActivityType.playing)
+        if not before_playing and after_playing and after.channel:
+            burgues = discord.utils.get(after.roles, name='Burguês')
+            if burgues:
+                game_details = f' - {after_playing.details}' if after_playing and after_playing.details else ''
+                notify(f'{after.nick or after.name} começou a jogar {after_playing.name}{game_details}')
 
 intents = discord.Intents(guilds=True, members=True, presences=True, voice_states=True)
 
