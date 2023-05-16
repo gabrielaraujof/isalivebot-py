0629e0ae10e1232a2b086f5f535f1bba75b99395
Gabriel Araujo <gabrielferreira@ciandt.com>
2023-05-16 00:58:48 -0300

    refact: message update

--- isalivebot.py
+++ isalivebot.py
@@ -10,13 +10,15 @@ class IsAlive(discord.Client):
     async def on_voice_state_update(self, member, before, after):
         name = member.nick or member.name
         playing = discord.utils.get(member.activities, type=discord.ActivityType.playing)
-        game_message = f' e está jogando {playing.name}' if playing else ''
+        game = f' e está jogando {playing.name}' if playing else ''
         game_details = f' - {playing.details}' if playing and playing.details else ''
+        game_message = f'{game}{game_details}'
+        channel = f'{after.channel.name}'
 
         if not before.channel and after.channel:
-            notify(f'{name} entrou em [{after.channel.name}]({after.channel.jump_url}){game_message}{game_details}')
+            notify(f'{name} entrou em {channel}{game_message}')
         elif not before.self_stream and after.self_stream:
-            notify(f'🔴 {name} está ao vivo em [{after.channel.name}]({after.channel.jump_url}) {game_message}{game_details}') 
+            notify(f'🔴 {name} está ao vivo em {channel}{game_message}') 
 
     async def on_presence_update(self, before, after):
         before_playing = discord.utils.get(before.activities, type=discord.ActivityType.playing)
